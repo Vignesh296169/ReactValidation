995b905c34d81ec8fe7f771e5d2ce2eb44dfabac
Vignesh <116133745+Vignesh296169@users.noreply.github.com>
2023-01-01 13:14:50 +0530

    vaild

--- src/Component/From/Form.js
+++ src/Component/From/Form.js
@@ -5,29 +5,55 @@ import Button from "../UI/Button";
 import { useState } from "react";
 function Form() {
 const[enteredtext,settext]=useState('');
+const[isvalid ,setvalid]=useState(true)
+const[iserror ,seterror]=useState(true)
   const textHandler=(event)=>{
+    if(event.target.value.trim().length>0){
+        setvalid(true)
+    }
      settext(event.target.value)
-  }
+  };
   const[enteredpassword,setpassword]=useState('');
   const passwordHandler=(event)=>{
+    if(event.target.value.trim().length>8){
+        seterror(true)
+    }
      setpassword(event.target.value)
   }
   const preventHandler=(event)=>{
       event.preventDefault();
+      if(enteredtext.trim().length===0 ){
+        setvalid(false)
+        return;
+      }
+      if (enteredpassword.trim().length<8){
+        seterror(false)
+        return;
+      }
+      console.log(enteredtext,enteredpassword)
+      setpassword('')
+      settext('')
   }
 
-
-  return (
+  const [inialert,setalert]=useState('')
+  const alertHandler=()=>{
+     
+  
+    }
+  
+  
+   return (
+    <>
     <Card className={styles.design}>
-
-      <form onSubmit={preventHandler}>
-        <label htmlFor="username">Username:</label>
+  <form onSubmit={preventHandler}>
+        <label style={{color:!isvalid && "red"}} htmlFor="username">Username:</label>
+          <p style={{color:"red"}}>{inialert}</p>
         <input value={enteredtext} onChange={textHandler} className={styles.text} id="username" type='text'/>
-        <label htmlFor="userPassword">password:</label>
-       <input value={enteredpassword} onChange={passwordHandler} className={styles.password} id="userPassword" type='password'/>
-        <Button>Submit</Button>
+        <label style={{color:!iserror ? "red":"black"}} htmlFor="userPassword">password:</label>
+       <input  value={enteredpassword} onChange={passwordHandler} className={styles.password} id="userPassword" type='password'/>
+        <Button onClick={alertHandler} >Submit</Button>
       </form>
-    </Card>
+    </Card></>
   );
 }
 
