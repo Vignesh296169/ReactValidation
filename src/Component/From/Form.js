2d2c896eae102a67c4752d5b4b4a7e84b60f463c
Vignesh <116133745+Vignesh296169@users.noreply.github.com>
2023-01-01 12:15:24 +0530

    clr

--- src/Component/From/Form.js
+++ src/Component/From/Form.js
@@ -1,16 +1,31 @@
 import React from "react";
 import Card from "../UI/Card";
 import styles from "./Form.module.css"
+import Button from "../UI/Button";
+import { useState } from "react";
 function Form() {
+const[enteredtext,settext]=useState('');
+  const textHandler=(event)=>{
+     settext(event.target.value)
+  }
+  const[enteredpassword,setpassword]=useState('');
+  const passwordHandler=(event)=>{
+     setpassword(event.target.value)
+  }
+  const preventHandler=(event)=>{
+      event.preventDefault();
+  }
+
+
   return (
     <Card className={styles.design}>
 
-      <form>
+      <form onSubmit={preventHandler}>
         <label htmlFor="username">Username:</label>
-        <input className={styles.text} id="username" type='text'/>
+        <input value={enteredtext} onChange={textHandler} className={styles.text} id="username" type='text'/>
         <label htmlFor="userPassword">password:</label>
-       <input className={styles.password} id="userPassword" type='password'/>
-        <button type="submit">submit</button>
+       <input value={enteredpassword} onChange={passwordHandler} className={styles.password} id="userPassword" type='password'/>
+        <Button>Submit</Button>
       </form>
     </Card>
   );
